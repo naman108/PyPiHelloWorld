d0616e83d439cfc7f53e975231e1c7cc7f27708e
naman108 <natha.paquette@gmail.com>
2020-06-16 11:15:41 -0300

    fe

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'testpackage',         # How you named your package folder (MyLib)
   packages = ['testpackage'],   # Chose the same as "name"
-  version = '0.8.5',      # Start with a small number and increase it with every change you make
+  version = '0.8.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Ghosty 1008',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/naman108/PyPiHelloWorld.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/naman108/PyPiHelloWorld/releases/tag/0.8.5',    # I explain this later on
+  download_url = 'https://github.com/naman108/PyPiHelloWorld/releases/tag/0.8.6',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'flask',
